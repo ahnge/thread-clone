704c66fc5430a23b0c8e1632673c02345bafb051
ahnge <ahnge226@gmail.com>
2023-08-03 15:37:17 +0630

    moved accounts into separate app

--- core/urls.py
+++ core/urls.py
@@ -17,12 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import register_view, login_view, logout_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
     path("", include("thread.urls")),
-    path("accounts/login/", login_view, name="login"),
-    path("accounts/logout/", logout_view, name="logout"),
-    path("accounts/register/", register_view, name="register"),
 ]
