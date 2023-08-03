5861ab5ef91fbbf2e7114dbefe46a78448cc41ab
ahnge <ahnge226@gmail.com>
2023-08-03 15:37:46 +0630

    the accounts app - login, logout, register

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import register_view, login_view, logout_view
+
+app_name = "accounts"
+
+urlpatterns = [
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("register/", register_view, name="register"),
+]
