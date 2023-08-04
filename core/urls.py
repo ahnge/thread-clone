4fb28160dae7dd77e68eef66761bf70056d35f6a
ahnge <ahnge226@gmail.com>
2023-08-04 13:53:14 +0630

    required html

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),
     path("", include("thread.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("admin/", admin.site.urls),
 ]
