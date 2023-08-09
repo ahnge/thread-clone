0f402a38e2342bbdfdb0fd3e304fd5c727819000
ahnge <ahnge226@gmail.com>
2023-08-09 10:09:28 +0630

    admin register

--- thread/admin.py
+++ thread/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Thread, TextThread, ImageThread, Follow, Comment, Like, Rethread
 
-# Register your models here.
+
+admin.site.register(Thread)
+admin.site.register(TextThread)
+admin.site.register(ImageThread)
+admin.site.register(Follow)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Rethread)
