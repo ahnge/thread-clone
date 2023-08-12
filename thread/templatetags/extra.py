490cd84d115546fabb70fe8c2845843085d2a8c7
ahnge <ahnge226@gmail.com>
2023-08-12 11:06:49 +0630

    change name

--- thread/templatetags/extra.py
+++ thread/templatetags/extra.py
@@ -3,8 +3,8 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name="cut")
-def cut(value):
+@register.filter(name="ecut")
+def ecut(value):
     """Get the first 2 characters of the value"""
     if len(value) <= 2:
         return value
