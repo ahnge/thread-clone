549fbb0c1583b7014764654d73423981a30f9e8f
ahnge <ahnge226@gmail.com>
2023-08-17 13:06:03 +0630

    add link field

--- accounts/models.py
+++ accounts/models.py
@@ -4,10 +4,11 @@ from django.contrib.auth.models import User
 
 class MyUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="myuser")
-    bio = models.CharField(max_length=200, blank=True)
+    bio = models.CharField(max_length=200, blank=True, null=True)
     profile_picture = models.ImageField(
         upload_to="profile_pictures/", blank=True, null=True
     )
+    link = models.CharField(max_length=64, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
