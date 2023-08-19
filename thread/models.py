236b4cae10213a34d3cfe91367215b62c9d43e80
ahnge <ahnge226@gmail.com>
2023-08-19 19:54:48 +0630

    add related name

--- thread/models.py
+++ thread/models.py
@@ -59,7 +59,7 @@ class Rethread(models.Model):
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment")
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
     text = models.TextField()
 
